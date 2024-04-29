25e42f6c1af70e0958f58520e5e4d638544b8f6b
aberry520 <aberry52098@gmail.com>
2024-04-28 21:42:44 -0400

    single quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,102 @@
-"use client";
-import styles from "./page.module.css";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import useWindowDimensions from "../../utils";
+'use client';
+import styles from './page.module.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import useWindowDimensions from '../../utils';
 export default function Home() {
   const { height, width } = useWindowDimensions();
   return (
     <>
       <div
         style={{
-          position: "absolute",
-          width: "100%",
+          position: 'absolute',
+          width: '100%',
           height: height,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <img
-          src="/familyLife-logo.png"
-          alt="Family Life Church Logo"
-          style={{ opacity: 0.1, height: "30em" }}
+          src='/familyLife-logo.png'
+          alt='Family Life Church Logo'
+          style={{ opacity: 0.1, height: '30em' }}
         />
       </div>
       <main
         className={styles.main}
-        // style={{ background: "red" }}
+        // style={{ background: 'red' }}
       >
         <div className={styles.description}>
           {width > 700 && (
             <>
-              <div style={{ width: "20%" }}>
+              <div style={{ width: '20%' }}>
                 <img
-                  src="/familyLife-logo.png"
-                  alt="Family Life Church Logo"
-                  style={{ width: "4.5em" }}
-                  // style={{ position: "absolute", opacity: 0.1, zIndex: 0 }}
+                  src='/familyLife-logo.png'
+                  alt='Family Life Church Logo'
+                  style={{ width: '4.5em' }}
+                  // style={{ position: 'absolute', opacity: 0.1, zIndex: 0 }}
                 />
               </div>
-              <div style={{ width: "59%", justifyContent: "center", display: "flex" }}>
+              <div
+                style={{
+                  width: '59%',
+                  justifyContent: 'center',
+                  display: 'flex',
+                }}
+              >
                 <img
-                  src="/familyLife-words.png"
-                  alt="Family Life Church Logo"
-                  style={{ height: "5em" }}
+                  src='/familyLife-words.png'
+                  alt='Family Life Church Logo'
+                  style={{ height: '5em' }}
                 />
               </div>
             </>
           )}
-          <div style={{ display: "flex", flexDirection: "row", justifyContent: width > 700 ? "flex-end" : "center" }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: width > 700 ? 'flex-end' : 'center',
+            }}
+          >
             <a
-              href="https://www.instagram.com/familylifechurchsc/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ marginLeft: 10 }}>
+              href='https://www.instagram.com/familylifechurchsc/'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ marginLeft: 10 }}
+            >
               <img
-                src="/instagram-rd-trs.png"
-                alt="Vercel Logo"
+                src='/instagram-rd-trs.png'
+                alt='Vercel Logo'
                 className={styles.vercelLogo}
-                width={"50em"}
+                width={'50em'}
               />
             </a>
             <a
-              href="https://www.facebook.com/people/Family-Life-Church-Iva/61558998081855/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ marginLeft: 10 }}>
+              href='https://www.facebook.com/people/Family-Life-Church-Iva/61558998081855/'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ marginLeft: 10 }}
+            >
               <img
-                src="/facebook-rd-trs.png"
-                alt="Vercel Logo"
+                src='/facebook-rd-trs.png'
+                alt='Vercel Logo'
                 className={styles.vercelLogo}
-                width={"50em"}
+                width={'50em'}
               />
             </a>
             <a
-              href="mailto:mike@familylifechurchsc.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ marginLeft: 10 }}>
+              href='mailto:mike@familylifechurchsc.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ marginLeft: 10 }}
+            >
               <img
-                src="/email-rd-trs.png"
-                alt="Vercel Logo"
+                src='/email-rd-trs.png'
+                alt='Vercel Logo'
                 className={styles.vercelLogo}
-                width={"50em"}
+                width={'50em'}
               />
             </a>
           </div>
@@ -89,14 +105,15 @@ export default function Home() {
         <div className={styles.center}>
           <h1
             style={{
-              textAlign: width > 700 ? "right" : "center",
-              fontSize: "5em",
-              width: width > 700 ? "49%" : "100%",
-            }}>
+              textAlign: width > 700 ? 'right' : 'center',
+              fontSize: '5em',
+              width: width > 700 ? '49%' : '100%',
+            }}
+          >
             <img
-              src="/welcome.png"
-              alt="Family Life Church Logo"
-              style={{ width: "5em" }}
+              src='/welcome.png'
+              alt='Family Life Church Logo'
+              style={{ width: '5em' }}
             />
             <br />
             {/* Welcome to
@@ -105,7 +122,7 @@ export default function Home() {
             <br />
             Church
           </h1>
-          <div style={{ width: width > 700 ? "49%" : "100%" }}>
+          <div style={{ width: width > 700 ? '49%' : '100%' }}>
             <Carousel
               autoPlay
               infiniteLoop
@@ -113,15 +130,19 @@ export default function Home() {
               showStatus={false}
               showIndicators={false}
               showThumbs={false}
-              interval={5000}>
+              interval={5000}
+            >
               <div>
-                <h2>Mission: "By the Word of God"</h2>
+                <h2>Mission: 'By the Word of God'</h2>
               </div>
               <div>
-                <h2>Vision: "Family finding life in Christ" </h2>
+                <h2>Vision: 'Family finding life in Christ' </h2>
               </div>
               <div>
-                <h2>Church DNA: Scriptural Fidelity, Family, Theology, Simplicity, Intentionality</h2>
+                <h2>
+                  Church DNA: Scriptural Fidelity, Family, Theology, Simplicity,
+                  Intentionality
+                </h2>
               </div>
             </Carousel>
           </div>
@@ -129,12 +150,10 @@ export default function Home() {
 
         <div
         // className={styles.grid}
-        // style={{ height: "20%", width: "100%" }}
+        // style={{ height: '20%', width: '100%' }}
         >
           <p>Statement of faith here</p>
-          <a
-            href="geo:124.028582,-29.201930"
-            target="_blank">
+          <a href='geo:124.028582,-29.201930' target='_blank'>
             Click here for map
           </a>
         </div>
