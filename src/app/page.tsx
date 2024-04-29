251cdf2cc7619a3a9da795938718f2529b62af46
aberry520 <aberry52098@gmail.com>
2024-04-28 21:31:49 -0400

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -27,11 +26,6 @@ export default function Home() {
         className={styles.main}
         // style={{ background: "red" }}
       >
-        {/* <img
-        src="/bg.png"
-        alt="Family Life Church Logo"
-        style={{ position: "absolute", zIndex: 0, top: 0, left: 0, right: 0, height: "100%" }}
-      /> */}
         <div className={styles.description}>
           {width > 700 && (
             <>
@@ -93,15 +87,6 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          {/* <Image
-          className={styles.logo}
-          src="/familyLife.png"
-          alt="Next.js Logo"
-          width={750}
-          height={450}
-          priority
-        /> */}
-
           <h1
             style={{
               textAlign: width > 700 ? "right" : "center",
@@ -121,12 +106,6 @@ export default function Home() {
             Church
           </h1>
           <div style={{ width: width > 700 ? "49%" : "100%" }}>
-            {/* <h2>Join us for worship!</h2>
-          <p>
-            Sunday Morning's
-            <br />
-            10:00 am
-          </p> */}
             <Carousel
               autoPlay
               infiniteLoop
@@ -152,62 +131,12 @@ export default function Home() {
         // className={styles.grid}
         // style={{ height: "20%", width: "100%" }}
         >
-          {/* <h1>Feel led </h1>
-        <h2>Join us for worship!</h2>
-        <p>
-          Sunday Morning Worship
-          <br />
-          10:00 am
-        </p> */}
           <p>Statement of faith here</p>
           <a
             href="geo:124.028582,-29.201930"
             target="_blank">
             Click here for map
           </a>
-          {/* <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Contact Us <span>-&gt;</span>
-          </h2>
-          <p>Reach out to our pastoral team and we will get back as soon as possible!</p>
-        </a> */}
-
-          {/* <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Giving <span>-&gt;</span>
-          </h2>
-          <p>Feel led to help finacially? You can donate online here!</p>
-        </a> */}
-
-          {/* <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Facebook <span>-&gt;</span>
-          </h2>
-          <p>Follow us on Facebook!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Instagram <span>-&gt;</span>
-          </h2>
-          <p>Follow us on Instagram!</p>
-        </a> */}
         </div>
       </main>
     </>
