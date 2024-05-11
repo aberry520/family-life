84d7e86dc9639cc8f9c50a23bf6c799d05ea30a2
aberry520 <aberry52098@gmail.com>
2024-05-11 00:11:19 -0400

    add fonts and make layput semi responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,95 @@
 'use client';
-import styles from './page.module.css';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import useWindowDimensions from '../../utils';
+import styles from "./page.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import useWindowDimensions from "../../utils";
 export default function Home() {
   const { height, width } = useWindowDimensions();
   return (
     <>
-      <div
-        style={{
-          position: 'absolute',
-          width: '100%',
-          height: height,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img
-          src='/familyLife-logo.png'
-          alt='Family Life Church Logo'
-          style={{ opacity: 0.1, height: '30em' }}
-        />
-      </div>
-      <main
-        className={styles.main}
-        // style={{ background: 'red' }}
-      >
+      {width < 500 && (
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: height,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <img
+            src="/familyLife-logo.png"
+            alt="Family Life Church Logo"
+            style={{ opacity: 0.1, height: "50em" }}
+          />
+        </div>
+      )}
+      <main className={styles.main}>
         <div className={styles.description}>
           {width > 700 && (
             <>
-              <div style={{ width: '20%' }}>
+              <div style={{ width: "20%" }}>
                 <img
-                  src='/familyLife-logo.png'
-                  alt='Family Life Church Logo'
-                  style={{ width: '4.5em' }}
+                  src="/familyLife-logo.png"
+                  alt="Family Life Church Logo"
+                  style={{ width: "4.5em" }}
                   // style={{ position: 'absolute', opacity: 0.1, zIndex: 0 }}
-                />
-              </div>
-              <div
-                style={{
-                  width: '59%',
-                  justifyContent: 'center',
-                  display: 'flex',
-                }}
-              >
-                <img
-                  src='/familyLife-words.png'
-                  alt='Family Life Church Logo'
-                  style={{ height: '5em' }}
                 />
               </div>
             </>
           )}
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: width > 700 ? 'flex-end' : 'center',
-            }}
-          >
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: width > 700 ? "flex-end" : "center",
+            }}>
             <a
-              href='https://www.instagram.com/familylifechurchsc/'
-              target='_blank'
-              rel='noopener noreferrer'
-              style={{ marginLeft: width > 700 ? 10 : 0 }}
-            >
+              href="https://www.instagram.com/familylifechurchsc/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginLeft: width > 700 ? 10 : 0 }}>
               <img
-                src='/instagram-rd-trs.png'
-                alt='Vercel Logo'
+                src="/instagram-rd-trs.png"
+                alt="Vercel Logo"
                 className={styles.vercelLogo}
-                width={'50em'}
+                width={"50em"}
               />
             </a>
             <a
-              href='https://www.facebook.com/people/Family-Life-Church-Iva/61558998081855/'
-              target='_blank'
-              rel='noopener noreferrer'
-              style={{ marginLeft: width > 700 ? 10 : 0 }}
-            >
+              href="https://www.facebook.com/people/Family-Life-Church-Iva/61558998081855/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginLeft: width > 700 ? 10 : 0 }}>
               <img
-                src='/facebook-rd-trs.png'
-                alt='Vercel Logo'
+                src="/facebook-rd-trs.png"
+                alt="Vercel Logo"
                 className={styles.vercelLogo}
-                width={'50em'}
+                width={"50em"}
               />
             </a>
             <a
-              href='mailto:mike@familylifechurchsc.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              style={{ marginLeft: width > 700 ? 10 : 0 }}
-            >
+              href="mailto:mike@familylifechurchsc.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginLeft: width > 700 ? 10 : 0 }}>
               <img
-                src='/email-rd-trs.png'
-                alt='Vercel Logo'
+                src="/email-rd-trs.png"
+                alt="Vercel Logo"
                 className={styles.vercelLogo}
-                width={'50em'}
+                width={"50em"}
               />
             </a>
             {width < 700 && (
               <a
-                href='https://www.google.com/maps/search/?api=1&query=2640%20Elberton%20Highway%2C%20Iva%2C%20SC%2029655'
-                target='_blank'
-                rel='noopener noreferrer'
-                style={{ marginLeft: width > 700 ? 10 : 0 }}
-              >
+                href="https://www.google.com/maps/search/?api=1&query=2640%20Elberton%20Highway%2C%20Iva%2C%20SC%2029655"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ marginLeft: width > 700 ? 10 : 0 }}>
                 <img
-                  src='/navigation-rd-trs.png'
-                  alt='Vercel Logo'
+                  src="/navigation-rd-trs.png"
+                  alt="Vercel Logo"
                   className={styles.vercelLogo}
-                  width={'50em'}
+                  width={"50em"}
                 />
               </a>
             )}
@@ -118,67 +97,48 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+          <img
+            src="/welcome.png"
+            alt="Family Life Church Logo"
+            style={{ width: "40em", marginBottom: "-3.25em" }}
+          />
           <h1
             style={{
-              textAlign: width > 700 ? 'right' : 'center',
-              fontSize: '5em',
-              width: width > 700 ? '49%' : '100%',
-            }}
-          >
-            <img
-              src='/welcome.png'
-              alt='Family Life Church Logo'
-              style={{ width: '5em' }}
-            />
-            <br />
-            {/* Welcome to
-            <br /> */}
-            Family Life
-            <br />
-            Church
+              textAlign: "center",
+              fontSize: "4em",
+              width: "100%",
+              color: "#0b4465",
+              fontFamily: "sinkin_sans600_semibold",
+            }}>
+            FAMILY LIFE CHURCH
           </h1>
-          <div style={{ width: width > 700 ? '49%' : '100%' }}>
-            <Carousel
-              autoPlay
-              infiniteLoop
-              showArrows={false}
-              showStatus={false}
-              showIndicators={false}
-              showThumbs={false}
-              interval={5000}
-            >
-              <div>
-                <h2>{`Mission: "By the Word of God"`}</h2>
-              </div>
-              <div>
-                <h2>{`Vision: "Family finding life in Christ"`} </h2>
-              </div>
-              <div>
-                <h2>
-                  Church DNA: Scriptural Fidelity, Family, Theology, Simplicity,
-                  Intentionality
-                </h2>
-              </div>
-            </Carousel>
-          </div>
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "2em",
+              width: "100%",
+              color: "#0b4465",
+              fontFamily: "sinkin_sans600_semibold",
+              fontWeight: 100,
+            }}>
+            BY THE WORD OF GOD
+          </h2>
         </div>
 
         <div
           // className={styles.grid}
           style={{
-            height: '20%',
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-          }}
-        >
+            height: "20%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}>
           <p>Statement of faith here</p>
           <a
-            href='https://www.google.com/maps/search/?api=1&query=2640%20Elberton%20Highway%2C%20Iva%2C%20SC%2029655'
-            target='_blank'
-          >
+            href="https://www.google.com/maps/search/?api=1&query=2640%20Elberton%20Highway%2C%20Iva%2C%20SC%2029655"
+            target="_blank">
             Click here for map
           </a>
         </div>
