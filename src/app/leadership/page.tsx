df89cb811d6ac33fdf18137b5dbd573a8d83e64f
aberry520 <aberry52098@gmail.com>
2025-03-06 20:10:38 -0500

    delete uneeded

--- src/app/leadership/page.tsx
+++ src/app/leadership/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Header from "@/components/header";
-import Home from "../../components/home";
 import Footer from "@/components/footer";
 export default function HomePage() {
   return (
--- src/components/events.tsx
+++ src/components/events.tsx
@@ -10,7 +10,6 @@ export default function Events() {
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">Upcoming Events</div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Join Us for Upcoming Events</h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Check out our upcoming events and activities. We have something for everyone, from worship services to
