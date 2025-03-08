620ec289202e58b356b9326ae9b617a1d8a2604e
aberry520 <aberry52098@gmail.com>
2025-03-08 09:05:05 -0500

    update import

--- src/app/leadership/page.tsx
+++ src/app/leadership/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import LeadershipSection from "../../../leadership-section";
+import LeadershipSection from "../../components/leadership-section";
 export default function HomePage() {
   return (
     <>
