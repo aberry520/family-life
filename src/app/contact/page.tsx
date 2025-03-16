b9c4eb425c3e21bf667e7827448093d991aac2af
aberry520 <aberry52098@gmail.com>
2025-03-15 23:40:32 -0400

    move gospel

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import Gospel from "@/components/gospel";
 import Contact from "@/components/contact";
 export default function HomePage() {
   return (
     <>
       <Header />
-
-      <Gospel />
       <Contact />
       <Footer />
     </>
